5e2fcef1699e073db24ec8447ba91f74307c1aa5
encobrain <encobrain@gmail.com>
2021-04-08 13:37:36 +0300

    Added base code for task

--- lib/client/task.go
+++ lib/client/task.go
@@ -1,5 +1,7 @@
 package client
 
+import "sync"
+
 // Task in any moment may be canceled. See Canceled()
 type Task interface {
 	UUID() string
@@ -8,7 +10,7 @@ type Task interface {
 	Status() string
 	// StatusSubscribe subscribes on actual task status from queue.
 	// If task state changed - previous task will be canceled.
-	// No need listen to task.Done() and status simultaniously.
+	// No need listen to task.Canceled() and status simultaniously.
 	// Empty status - task not exists.
 	// Closed chan - client stopped
 	StatusSubscribe() (status <-chan Task)
@@ -31,4 +33,77 @@ type Task interface {
 	// Reject rejects task for to do by another worker.
 	// Method should use if task received by queue.TasksSubscribe().
 	Reject() (done <-chan struct{})
+}
+
+func taskNew() *task {
+	return &task{
+		canceled: make(chan struct{}),
+	}
+}
+
+type task struct {
+	stateId    uint64
+	uuid       string
+	parentUUID string
+	status     string
+
+	mu       sync.Mutex
+	canceled chan struct{}
+	err      error
+}
+
+func (t *task) cancel(reason error) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
+	if t.err != nil {
+		return
+	}
+
+	t.err = reason
+
+	close(t.canceled)
+}
+
+func (t *task) UUID() string {
+	return t.uuid
+}
+
+func (t *task) ParentUUID() string {
+	return t.parentUUID
+}
+
+func (t *task) Status() string {
+	return t.status
+}
+
+func (t *task) StatusSubscribe() (status <-chan Task) {
+
+}
+
+func (t *task) Content() (content <-chan []byte) {
+
+}
+
+func (t *task) StatusSet(status string, content []byte) (done <-chan bool) {
+
+}
+
+func (t *task) Remove() (done <-chan bool) {
+
+}
+
+func (t *task) Canceled() (canceled <-chan struct{}) {
+	return t.canceled
+}
+
+func (t *task) Err() (err error) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
+	return t.err
+}
+
+func (t *task) Reject() (done <-chan struct{}) {
+
 }
