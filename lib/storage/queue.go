00af96546df888c19a06bf5bda43c0e910c80eb7
encobrain <encobrain@gmail.com>
2021-04-12 17:45:01 +0300

    Added code for storage Queue & Task interfaces

--- lib/storage/queue.go
+++ lib/storage/queue.go
@@ -1,5 +1,7 @@
 package storage
 
+import "sync"
+
 type Queue interface {
 	ID() uint64
 	TaskNew(parentUUID string, status string, content []byte) Task
@@ -17,4 +19,128 @@ type Queue interface {
 	// TaskRemove removes task from queue.
 	// If false - task not exists
 	TaskRemove(uuid string) (ok bool)
+}
+
+func NewQueue(stor Storage, info *QueueInfo) Queue {
+	q := &queue{
+		storage: stor,
+		info:    info,
+	}
+
+	return q
+}
+
+type queue struct {
+	info    *QueueInfo
+	storage Storage
+
+	mu   sync.Mutex
+	task map[string]*task
+}
+
+func (q *queue) ID() uint64 {
+	return q.info.Id
+}
+
+func (q *queue) TaskNew(parentUUID string, status string, content []byte) Task {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	ti := q.storage.TaskNew(q.info.Id, parentUUID, status, content)
+
+	t := NewTask(ti)
+
+	q.task[ti.UUID] = t.(*task)
+
+	return t
+}
+
+func (q *queue) taskGet(uuid string) Task {
+	t := q.task[uuid]
+
+	if t == nil {
+		ti := q.storage.TaskGet(q.info.Id, uuid)
+
+		if ti == nil {
+			return nil
+		}
+
+		t = NewTask(ti).(*task)
+
+		q.task[ti.UUID] = t
+	}
+
+	return t
+}
+
+func (q *queue) TaskGet(uuid string) Task {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	return q.taskGet(uuid)
+}
+
+func (q *queue) TasksGet() (tasks []Task) {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	tis := q.storage.TasksGet(q.info.Id)
+
+	for _, ti := range tis {
+		t := q.task[ti.UUID]
+
+		if t == nil {
+			t = NewTask(ti).(*task)
+			q.task[ti.UUID] = t
+		}
+
+		tasks = append(tasks, t)
+	}
+
+	return
+}
+
+func (q *queue) TaskStatusSet(uuid string, status string, content []byte) Task {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	it := q.taskGet(uuid)
+
+	if it == nil {
+		return nil
+	}
+
+	if !q.storage.TaskStatusSet(q.info.Id, uuid, status, content) {
+		return nil
+	}
+
+	t := it.(*task)
+
+	t = t.statusSet(status, content)
+
+	q.task[uuid] = t
+
+	return t
+}
+
+func (q *queue) TaskRemove(uuid string) (ok bool) {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	it := q.taskGet(uuid)
+
+	if it == nil {
+		return
+	}
+
+	if !q.storage.TaskRemove(q.info.Id, uuid) {
+		return
+	}
+
+	t := it.(*task)
+	t.cancel()
+
+	delete(q.task, uuid)
+
+	return true
 }
--- lib/storage/task.go
+++ lib/storage/task.go
@@ -9,3 +9,54 @@ type Task interface {
 	Status() string
 	Content() []byte
 }
+
+func NewTask(info *TaskInfo) Task {
+	t := &task{
+		info:     info,
+		canceled: make(chan struct{}),
+	}
+
+	return t
+}
+
+type task struct {
+	info     *TaskInfo
+	canceled chan struct{}
+}
+
+func (t *task) cancel() {
+	defer func() { recover() }()
+	close(t.canceled)
+}
+
+func (t *task) statusSet(status string, content []byte) *task {
+	t.cancel()
+
+	return NewTask(&TaskInfo{
+		QueueId:    t.info.QueueId,
+		UUID:       t.info.UUID,
+		ParentUUID: t.info.ParentUUID,
+		Status:     status,
+		Content:    content,
+	}).(*task)
+}
+
+func (t *task) Canceled() <-chan struct{} {
+	return t.canceled
+}
+
+func (t *task) UUID() string {
+	return t.info.UUID
+}
+
+func (t *task) ParentUUID() string {
+	return t.info.ParentUUID
+}
+
+func (t *task) Status() string {
+	return t.info.Status
+}
+
+func (t *task) Content() []byte {
+	return t.info.Content
+}
