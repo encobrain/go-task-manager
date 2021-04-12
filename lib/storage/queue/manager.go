dde574bd7ecc019eebfb25fabd51a301507ddfe2
encobrain <encobrain@gmail.com>
2021-04-12 17:01:06 +0300

    Added storage queue manager code

--- lib/storage/queue/manager.go
+++ lib/storage/queue/manager.go
@@ -1,6 +1,9 @@
 package queue
 
-import "github.com/encobrain/go-task-manager/lib/storage"
+import (
+	"github.com/encobrain/go-task-manager/lib/storage"
+	"sync"
+)
 
 type Manager interface {
 	// GetOrCreate gets queue by name or creates it if not exists
@@ -8,4 +11,49 @@ type Manager interface {
 	// Get gets queue by id.
 	// If nil - queue not exists
 	Get(id uint64) storage.Queue
+}
+
+func NewManager(stor storage.Storage) *manager {
+	m := &manager{
+		storage: stor,
+	}
+
+	m.queue.byName = map[string]storage.Queue{}
+	m.queue.byId = map[uint64]storage.Queue{}
+
+	return m
+}
+
+type manager struct {
+	storage storage.Storage
+
+	mu    sync.Mutex
+	queue struct {
+		byName map[string]storage.Queue
+		byId   map[uint64]storage.Queue
+	}
+}
+
+func (m *manager) GetOrCreate(name string) storage.Queue {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	q := m.queue.byName[name]
+
+	if q == nil {
+		qi := m.storage.QueueGetOrCreate(name)
+
+		q = storage.NewQueue(m.storage, qi)
+		m.queue.byName[name] = q
+		m.queue.byId[qi.Id] = q
+	}
+
+	return q
+}
+
+func (m *manager) Get(id uint64) storage.Queue {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	return m.queue.byId[id]
 }
