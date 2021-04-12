dfb58c8dbb2ce0b0257e5f80fff3a59d71e7a937
encobrain <encobrain@gmail.com>
2021-04-12 21:19:57 +0300

    Changed options names and added apply

--- model/config/process.go
+++ model/config/process.go
@@ -1,15 +1,25 @@
 package config
 
+import "runtime"
+
 type Process struct {
-	Name string `yaml:"name"   env:"PROCESS_NAME"   long:"process.name"   description:"Process name"`
+	Name string `yaml:"name"   env:"PROCESS_NAME"   long:"name"   description:"Process name"`
 	Run  struct {
-		PidPathfile string `yaml:"pid_pathfile"   env:"PROCESS_PID_PATHFILE"   long:"pidPathfile"   description:"Pathfile for store process id"`
-	} `yaml:"run" group:"Run options" namespace:"run"`
+		PidPathfile string `yaml:"pid_pathfile"   env:"PROCESS_PID_PATHFILE"   long:"run.pidPathfile"   description:"Pathfile for store process id"`
+	} `yaml:"run" namespace:"run"`
 	Threads struct {
-		Max int `yaml:"max"   env:"PROCESS_THREADS_MAX"   long:"process.threads.max"   description:"Use max threads. 0=cpu count"   default:"0"`
-	} `yaml:"threads" group:"Threads options" namespace:"threads"`
+		Max int `yaml:"max"   env:"PROCESS_THREADS_MAX"   long:"threads.max"   description:"Use max threads. 0=cpu count"   default:"0"`
+	} `yaml:"threads"`
 }
 
 func (p Process) Pathfile() string {
 	return "process.yaml"
+}
+
+func (p *Process) Apply() {
+	c := p.Threads.Max
+	if c == 0 {
+		c = runtime.NumCPU()
+	}
+	runtime.GOMAXPROCS(c)
 }
