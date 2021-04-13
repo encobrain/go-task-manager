05171ecd1a9b593345e6ba734ffb614814aa21e4
encobrain <encobrain@gmail.com>
2021-04-13 12:14:44 +0300

    Added default config values for server

--- model/config/server.go
+++ model/config/server.go
@@ -2,9 +2,9 @@ package config
 
 type Server struct {
 	Listen struct {
-		Ip   string `yaml:"host" env:"LISTEN_IP" long:"listen.ip"   description:"Listen ip"`
-		Port string `yaml:"port" env:"LISTEN_PORT" long:"listen.port"   description:"Listen port"`
-		Path string `yaml:"path" env:"LISTEN_PATH" long:"listen.path" description:"Listen path"`
+		Ip   string `yaml:"host" env:"LISTEN_IP" long:"listen.ip"   description:"Listen ip" default:"0.0.0.0"`
+		Port string `yaml:"port" env:"LISTEN_PORT" long:"listen.port"   description:"Listen port" default:"8080"`
+		Path string `yaml:"path" env:"LISTEN_PATH" long:"listen.path" description:"Listen path" default:"/task-manager-server"`
 	} `yaml:"listen"`
 }
 
