8991ce0723a0d25a90a73e5966c3a958b369bbc3
encobrain <encobrain@gmail.com>
2021-04-13 19:47:51 +0300

    Added client conf parse opts

--- model/config/client.go
+++ model/config/client.go
@@ -2,9 +2,13 @@ package config
 
 type Client struct {
 	Connect struct {
-		Scheme string
-		Host   string
-		Port   int
-		Path   string
-	}
+		Scheme string `yaml:"scheme" long:"connect.scheme" description:"Scheme for connect" default:"ws"`
+		Host   string `yaml:"host" long:"connect.host" description:"Server host" default:"localhost"`
+		Port   int    `yaml:"port" long:"connect.port" description:"Server port" default:"8080"`
+		Path   string `yaml:"path" long:"connect.path" description:"Listen path" default:"/task-manager-server"`
+	} `yaml:"connect"`
+}
+
+func (Client) Pathfile() string {
+	return "client.yaml"
 }
