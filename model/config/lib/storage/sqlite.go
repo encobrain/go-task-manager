a725304d33814da91132dc80d07833cdd1308760
encobrain <encobrain@gmail.com>
2021-04-13 16:03:00 +0300

    Corrected configs

--- model/config/lib/storage/sqlite.go
+++ model/config/lib/storage/sqlite.go
@@ -1,7 +1,13 @@
 package storage
 
+import "github.com/encobrain/go-task-manager/lib/filepath"
+
 type SQLite struct {
 	Db struct {
-		Path string
-	}
+		Path string `yaml:"path" long:"db.path" description:"Path to storage db" default:"./storage.db.sqlite3"`
+	} `yaml:"db"`
+}
+
+func (s *SQLite) Init() {
+	s.Db.Path = filepath.Resolve(true, s.Db.Path)
 }
