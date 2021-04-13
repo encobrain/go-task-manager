50a8347c82305a2bfd26fccae138d6fb70668279
encobrain <encobrain@gmail.com>
2021-04-13 16:21:56 +0300

    Added start code

--- cli/task-manager-server/cmd-start.go
+++ cli/task-manager-server/cmd-start.go
@@ -1,16 +1,25 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"github.com/encobrain/go-context.v2"
 	"github.com/encobrain/go-task-manager/cli"
+	contextSupp "github.com/encobrain/go-task-manager/lib/context"
+	"github.com/encobrain/go-task-manager/lib/storage/queue"
+	"github.com/encobrain/go-task-manager/lib/storage/sqlite"
+	"github.com/encobrain/go-task-manager/model/config/service"
+	task_manager "github.com/encobrain/go-task-manager/service/task-manager"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+	"time"
 )
 
 type CmdStart struct {
 	cli.CmdStart
 }
 
-func (c CmdStart) Execute(args []string) error {
+func (c CmdStart) Execute(args []string) (err error) {
 	conf.Init()
 	success, err := c.CmdStart.Execute(conf.Process.Run.PidPathfile)
 
@@ -18,5 +27,125 @@ func (c CmdStart) Execute(args []string) error {
 		return fmt.Errorf("execute start fail. %s", err)
 	}
 
-	return errors.New("not implemented")
+	startCtx := context.Main.Child("start", c.start).Go()
+
+	go func() {
+		sig := <-c.CmdStart.ShutdownSignals()
+
+		log.Printf("Received signal: %s\n", sig.String())
+
+		startCtx.Cancel(fmt.Errorf("service shutdown"))
+	}()
+
+	var prevRuns string
+
+	for {
+		select {
+		case <-context.Main.ChildsFinished(true):
+			log.Printf("Service stopped\n")
+			return
+		case <-startCtx.Done():
+			select {
+			case <-context.Main.ChildsFinished(true):
+				continue
+			case <-time.After(time.Second):
+				currRuns := contextSupp.GetDeadlocksInfo(context.Main)
+
+				if currRuns != prevRuns {
+					log.Printf("Waiting for close contexts:\n%s\n", currRuns)
+					prevRuns = currRuns
+				}
+			}
+		}
+	}
+}
+
+func (c CmdStart) panicHandler(ctx context.Context, panicErr interface{}) {
+	log.Printf("Service panic. %s\n", panicErr)
+
+	ctx.Cancel(fmt.Errorf("service panic"))
+}
+
+func (c CmdStart) start(ctx context.Context) {
+	ctx.PanicHandlerSet(c.panicHandler)
+
+	serverCtx := ctx.Child("server", c.startServer).
+		ValueSet("config", &service.TaskManager{})
+
+	stor := sqlite.New(&conf.Storage)
+	sqm := queue.NewManager(stor)
+
+	ctx.ValueSet("storage.queue.manager", sqm)
+
+	stor.Start()
+	serverCtx.Go()
+
+	<-ctx.Done()
+
+	log.Printf("Stopping service...\n")
+
+	stor.Stop()
+}
+
+func (c CmdStart) startServer(ctx context.Context) {
+	tmService := task_manager.New(ctx)
+	tmService.Start()
+
+	servMux := &http.ServeMux{}
+	upgrader := websocket.Upgrader{}
+
+	http.HandleFunc(conf.Server.Listen.Path, func(w http.ResponseWriter, r *http.Request) {
+		conn, err := upgrader.Upgrade(w, r, nil)
+
+		if err != nil {
+			log.Printf("Upgrade connection fail. %s\n", err)
+			w.WriteHeader(http.StatusUpgradeRequired)
+			_, err = w.Write([]byte("Upgrade connection fail"))
+		} else {
+			err = tmService.ConnServe(conn)
+
+			if err != nil {
+				log.Printf("Serve connection fail. %s\n", err)
+
+				w.WriteHeader(http.StatusInternalServerError)
+				_, err = w.Write([]byte("Serve connection fail"))
+			}
+		}
+
+		if err != nil {
+			log.Printf("Write response fail. %s\n", err)
+		}
+	})
+
+	addr := fmt.Sprintf("%s:%d", conf.Server.Listen.Ip, conf.Server.Listen.Port)
+
+	server := http.Server{
+		Addr:    addr,
+		Handler: servMux,
+	}
+
+	ctx.Child("listen", func(_ context.Context) {
+		log.Printf("HTTP Server started listening on %s\n", addr)
+
+		err := server.ListenAndServe()
+
+		if err == http.ErrServerClosed {
+			log.Printf("HTTP server closed\n")
+
+			return
+		}
+
+		panic(fmt.Errorf("server listen fail. %s", err))
+	}).Go()
+
+	<-ctx.Done()
+
+	log.Printf("Closing HTTP server...\n")
+
+	err := server.Close()
+
+	if err != nil {
+		log.Printf("Close HTTP server fail. %s\n", err)
+	}
+
 }
